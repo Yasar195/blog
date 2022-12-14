a8b4ad8d82baf86a040c7809222808f58bb0b1df
Yasar195 <imyasar07@gmail.com>
2022-12-14 12:21:39 +0530

    added text box

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import panda from '../assets/images/panda.webp';
 import Nav from '../components/Nav'
+import { BsArrowDown } from 'react-icons/bs';
 
 export default function Home() {
   return (
@@ -16,8 +17,14 @@ export default function Home() {
         <Nav/>
         <div className='flex p-8 w-full h-5/6 flex-col flex-col-reverse sm:flex-row'>
           <div className={`${styles.div2} opacity-0 w-full h-2/4 flex justify-evenly flex-col sm:h-full sm:w-2/4`}>
-            <h1 className='sm: text-2xl md:text-4xl lg:text-5xl w-4/5'>Be a better developer read good articles</h1>
-            <h1 className='sm: text-1xl md:text-3xl lg:text-4xl w-4/5 text-neutral-400'>Find all my writings here</h1>
+            <h1 className='sm: text-2xl md:text-4xl lg:text-5xl'>Be a better developer read good articles</h1>
+            <h1 className='sm: text-1xl md:text-3xl lg:text-4xl text-neutral-400'>Find all my writings here</h1>
+            <div className='w-full mt-4 flex items-center justify-evenly gap-2'>
+              <input className='w-3/4 p-4 outline-0 border-2 rounded-full bg-transparent text-xl lg:p-8 lg:text-2xl' type="text" placeholder='Search articles'/>
+              <div className='p-4 rounded-full flex items-center bg-neutral-400 justify-center'>
+                <BsArrowDown size={'30px'}/>
+              </div>
+            </div>
           </div>
           <div className={`${styles.div1} opacity-0 w-full h-2/4 flex justify-evenly items-center sm:h-full sm:w-2/4`}>
             <Image className='w-3/5' src={panda} alt="panda"/>
