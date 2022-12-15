886cf5abae3ae075fb2dc24cfc9743f8f3ffc7a1
Yasar195 <imyasar07@gmail.com>
2022-12-15 14:49:48 +0530

    blog ui

---
+++ components/Thumbnail.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import docker from "../assets/images/docker.jpg";
+import { GoCalendar } from 'react-icons/go';
+
+export default function Thumbnail(){
+    return (
+        <div className="w-full h-fit p-6 md:w-4/6 md:p-8 lg:w-2/6 drop-shadow-xl border-2 rounded-2xl border-neutral-400">
+            <Image className="w-full rounded-lg" src={docker} alt="img"/>
+            <h1 className="mt-6 text-xl md:text-2xl">Introduction Docker containerisation and managing it with kubernetes cluster</h1>
+            <div className="mt-6 w-full flex items-center justify-start">
+                <div className="flex w-fit items-center justify-start gap-2">
+                    <GoCalendar/>
+                    <p className="mt-1 text-neutral-400">11-12-2022</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 import panda from '../assets/images/panda.webp';
 import Nav from '../components/Nav'
 import { BsArrowDown } from 'react-icons/bs';
+import Thumbnail from '../components/Thumbnail';
 
 export default function Home() {
   return (
@@ -17,17 +18,29 @@ export default function Home() {
         <Nav/>
         <div className='flex p-8 w-full h-5/6 flex-col flex-col-reverse sm:flex-row'>
           <div className={`${styles.div2} opacity-0 w-full h-2/4 flex justify-evenly flex-col sm:h-full sm:w-2/4`}>
-            <h1 className='sm: text-2xl md:text-4xl lg:text-5xl'>Be a better developer read good articles</h1>
-            <h1 className='sm: text-1xl md:text-3xl lg:text-4xl text-neutral-400'>Find all my writings here</h1>
-            <div className='w-full mt-4 flex items-center justify-evenly gap-2'>
-              <input className='w-3/4 p-4 outline-0 border-2 rounded-full bg-transparent text-xl lg:p-8 lg:text-2xl' type="text" placeholder='Search articles'/>
-              <div className='p-4 rounded-full flex items-center bg-neutral-400 justify-center'>
+            <h1 className='text-2xl md:text-4xl lg:text-5xl'>Be a better developer read good articles</h1>
+            <h1 className='text-1xl md:text-3xl lg:text-4xl text-neutral-400'>Find all my writings here</h1>
+            <div className='w-full mt-4 flex items-center justify-start gap-4'>
+              <input className='w-3/4 p-4 outline-0 border-2 rounded-full bg-transparent text-md lg:p-8 lg:text-2xl' type="text" placeholder='Search articles'/>
+              <a href='#articles'><div className='p-4 rounded-full flex items-center transition-all hover:bg-neutral-400 justify-center'>
                 <BsArrowDown size={'30px'}/>
-              </div>
+              </div></a>
             </div>
           </div>
           <div className={`${styles.div1} opacity-0 w-full h-2/4 flex justify-evenly items-center sm:h-full sm:w-2/4`}>
             <Image className='w-3/5' src={panda} alt="panda"/>
+          </div>
+        </div>
+      </main>
+      <main className='w-full h-full' id="articles">
+        <div className='p-8 w-full h-full'>
+          <div className='w-full h-1/5'>
+            <h1 className='text-2xl md:text-4xl lg:text-5xl'>New articles</h1>
+          </div>
+          <div className='w-full h-4/5 flex gap-8 items-center justify-start flex-col lg:flex-row'>
+            <Thumbnail/>
+            <Thumbnail/>
+            <Thumbnail/>
           </div>
         </div>
       </main>
